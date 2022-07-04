bb68de5ce196283e6989fd5655ab1dbdf44acb11
Endre Barabas <barabasandrew@gmail.com>
2022-07-04 16:22:32 +0100

    HERE WE GOOOOOOO

---
+++ src/test/java/com/bethappy/demo/FantasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bethappy.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
