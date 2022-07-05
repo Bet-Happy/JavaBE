7ad9c82cd3f5219dd55a98facf3416657112a679
Endre Barabas <barabasandrew@gmail.com>
2022-07-05 12:28:48 +0100

    asdf

--- src/main/java/com/bethappy/demo/controller/CharacterController.java
+++ src/main/java/com/bethappy/demo/controller/CharacterController.java
@@ -1,9 +1,34 @@
 package com.bethappy.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 // ADDED
 import com.bethappy.demo.model.*;
 import com.bethappy.demo.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+// NOT NECESSARY FOR NOW
+import org.springframework.web.servlet.view.RedirectView;
+import java.security.Principal;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 public class CharacterController {
   
+  @Autowired
+  CharacterRepository characterRepository;
+
+  @GetMapping("/")
+  public String main(Model model){
+    return "/";
+  }
+
+  // @PostMapping("/")
+  // public RedirectView CREATIONPLACEHOLDER(@ModelAttribute Characters character, Model model) {
+
+  // }
 }
