0037ef3455772429b38353e1817b0523334e95cf
Demis Lavrentidis <demilavr@gmail.com>
2022-07-06 10:24:01 +0100

    saved

--- src/test/java/com/bethappy/demo/model/ResourcesTest.java
+++ src/test/java/com/bethappy/demo/model/ResourcesTest.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 public class ResourcesTest {
-  private Resource copper = new Resource("Copper", 5, 5);
+  private Resource copper = new Resource("Copper", 5, 3);
   
   @Test
-  public void resourceHasName(){
+  public void resourceHasValues(){
     assertThat(copper.getName()).isEqualTo("Copper");
+    assertThat(copper.getXP()).isEqualTo(5);
+    assertThat(copper.getTPA()).isEqualTo(3);
   }
 } 
