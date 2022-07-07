47418c7ea5a088262e58352c1d30a1ee00cf2a55
winstoncwang <congwang@live.co.uk>
2022-07-07 13:05:17 +0100

    missing controller

--- src/main/java/com/bethappy/demo/controller/CharactersController.java
+++ src/main/java/com/bethappy/demo/controller/CharactersController.java
@@ -1,51 +1,15 @@
 package com.bethappy.demo.controller;
 
-// import java.util.List;
-
 import com.bethappy.demo.model.Characters;
 import com.bethappy.demo.repository.CharactersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
-
-/*
- ADDED
-import org.springframework.beans.factory.annotation.Autowired;
-*/
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.ui.Model;
 import com.bethappy.demo.service.ResponseHandler;
-//JSON HANDLING
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-// NOT NECESSARY FOR NOW
-import org.springframework.web.servlet.view.RedirectView;
-import org.springframework.web.bind.annotation.RequestParam;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.http.HttpStatus;
-
-// // ADDED
-// import com.bethappy.demo.model.*;
-// import com.bethappy.demo.repository.*;
-// //import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Controller;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.ModelAttribute;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.ui.Model;
-// import com.bethappy.demo.service.ResponseHandler;
-// //JSON HANDLING
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// // NOT NECESSARY FOR NOW
-// import org.springframework.web.servlet.view.RedirectView;
-// import org.springframework.web.bind.annotation.RequestParam;
 
  @Controller
  public class CharactersController {
@@ -63,22 +27,10 @@ import org.springframework.web.bind.annotation.RequestParam;
     }
   }
 
-
   @PostMapping("/character")
   public ResponseEntity<?> characterCreation(@RequestBody Characters character){
     characterRepository.save(character);
     return ResponseHandler.generateResponse(HttpStatus.OK, true, "Character was created", character);
   }
-
-   //   @PostMapping("/")
-//   //how will React.js will request data? Do we get a request with this string inside?
-//   public ResponseEntity<?> main(){
-//     //Characters character = (Characters)characterRepository.findById(Long.valueOf(1));
-//     Characters character = characterRepository.findById(Long.valueOf(1));
-//     return ResponseHandler.generateResponse(HttpStatus.OK, true, "Character was created", character);
-//   }
-
-// }
-
 }
 
